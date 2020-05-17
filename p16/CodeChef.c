026537007604ce3b50a93f18bee633b8ef7779bf
Moisés Fernández <moy-881@hotmail.com>
2020-05-17 18:01:18 -0500

    Added P16

---
+++ p16/CodeChef.c
@@ -0,0 +1,32 @@
+// Moisés Fernández Zárate A01197049
+// 17/05/2020
+
+#include <stdio.h>
+
+int mainCodeChef() {
+    int T, i, n;
+    double ans, aux;
+
+    FILE *in = fopen("input.txt", "r");
+    FILE *out = fopen("output.txt", "w");
+
+    fscanf(in, "%d", &T);
+
+    while(T--) {
+        fscanf(in, "%d", &n);
+
+        ans = 0;
+        aux = n;
+
+        for(i = 1; i <= n; i++) {
+            ans = ans + aux / i;
+        }
+        
+        fprintf(out, "%.1f\n", ans);
+    }
+
+    fclose(in);
+    fclose(out);
+
+    return 0;
+} 
